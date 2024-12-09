576e8515401d428643be1e43ea0da483d123d4da
peterkrglv <peterkrglv@gmail.com>
2024-12-10 02:22:58 +0300

    IDE Initial commit

--- metro_server/metro/settings.py
+++ metro_server/metro/settings.py
@@ -77,15 +77,14 @@ WSGI_APPLICATION = 'metro.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': "metro",
-    #     "USER": "postgres",
-    #     "PASSWORD": "991004",
-    #     "HOST": "localhost",
-    #     "PORT": "5432"
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "metro",
+        "USER": "postgres",
+        "PASSWORD": "991004",
+        "HOST": "localhost",
+        "PORT": "5432"
+    }
 }
 
 
